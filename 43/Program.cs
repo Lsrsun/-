fe3880493d115044fcacf182ed8b688c207d7872
Regina <mustafinaregina2004@gmail.com>
2023-03-19 14:34:32 +0300

    43

---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = a * x + c, y = b * x + d; значения c, a, d и b задаются пользователем.
+
+// c = 2, a = 5, d = 4, b = 9 -> (-0,5; -0,5)
+
+double Input(string msg)
+{
+    Console.WriteLine(msg);
+    double N = Convert.ToDouble(Console.ReadLine());
+    return N;
+}
+
+void Point(double a, double b, double c, double d)
+{
+    double X = (d-c)/(a-b);
+    double Y = (a * ((d-c)/(a-b)))+c;
+    Console.WriteLine($"Точка пересечения: ({X}, {Y})");
+}
+
+double a = Input("Введите a");
+double b = Input("Введите b");
+double c = Input("Введите c");
+double d = Input("Введите d");
+Point(a, b, c, d);
+
